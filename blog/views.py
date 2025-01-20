fc731f5b9b05b7e5b107457ddc14d7ec6c684395
Zakariye Farah Nor <0e6xrww7@students.codeinstitute.net>
2025-01-20 15:43:50 +0000

    Add the first views for the admin page

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views import generic
+from .models import Post
 
 # Create your views here.
-def my_blog(request):
-    if request.method == "POST":
-        return HttpResponse("You Posted")
-    elif request.method == "GET":
-        return HttpResponse("You got the data")
+class PostList(generic.ListView):
+    queryset = Post.objects.filter(status=1)
